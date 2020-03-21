a6c71d461cf200688c7829dd1bc9349a86d1e18b
alisonamerico <alison.americo@gmail.com>
2020-03-21 13:15:13 -0300

    Adicionada docstring

--- 01_estrutura_sequencial/08_calcula_salario.py
+++ 01_estrutura_sequencial/08_calcula_salario.py
@@ -5,12 +5,22 @@ Calcule e mostre o total do seu salário no referido mês.
 """
 
 
-def calcula_salario(valor_1_hora, valor_horas_mes):
+def calcula_salario(valor_1_hora: int, valor_horas_mes: int) -> int:
+    """Função que calcula salário de acordo com as horas trabalhas no mês.
+    
+    Arguments:
+        valor_1_hora {int} -- Valor de uma hora trabalhada.
+        valor_horas_mes {int} -- Quantidade de horas trabalhadas no mês.
+    
+    Returns:
+        int -- Retorno o salaário do mês.
+    """
     total_salario_mes = valor_1_hora * valor_horas_mes
     return total_salario_mes
 
 
 def test_calcula_salario() -> None:
+    """test_calcula_salario(8, 32) -> 256"""
     valor_entrada_1_hora = 8
     valor_entrada_horas_por_mes = 32
     valor_esperado_total_salario_mes = (
