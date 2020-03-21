cdcffd37c69926942d9c5205245fa8400a6af1b4
alisonamerico <alison.americo@gmail.com>
2020-03-21 16:52:58 -0300

    Adicionada docstring

--- 01_estrutura_sequencial/10_celsius_farenheit.py
+++ 01_estrutura_sequencial/10_celsius_farenheit.py
@@ -9,11 +9,20 @@ Fórmula:
 
 
 def celsius_farenheit(valor_celsius: float) -> float:
+    """Função que converte temperatura de Celsius para graus Farenheit
+    
+    Arguments:
+        valor_celsius {float} -- valor de temperatura em graus Celsius
+    
+    Returns:
+        float -- Retorna o valor de temperatura em Farenheit
+    """
     valor_farenheit = ((9 * valor_celsius) / 5) + 32
     return valor_farenheit
 
 
 def test_celsius_farenheit() -> None:
+    """test_celsius_farenheit(valor_celsius) -> valor_farenheit"""
     valor_entrada_celsius = 1
     valor_esperado_farenheit = 33.80000
     assert celsius_farenheit(valor_entrada_celsius) == valor_esperado_farenheit
