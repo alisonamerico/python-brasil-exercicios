0ce5b5b6002db95562f52e965b2e014c5b3edb4a
alisonamerico <alison.americo@gmail.com>
2020-03-28 22:25:05 -0300

    adicionada docstring

--- 02_estrutura_de_decisao/04_letra_vogal_ou_consoante.py
+++ 02_estrutura_de_decisao/04_letra_vogal_ou_consoante.py
@@ -4,6 +4,14 @@
 
 
 def valida_letra_se_eh_vogal_ou_consoante(letra: str) -> str:
+    """Função que verifica se letra informada é uma vogal ou consoante.
+    
+    Arguments:
+        letra {str} -- letra a ser verificada.
+    
+    Returns:
+        str -- Retorna uma vogal ou consoante.
+    """
     vogais = ["a", "e", "i", "o", "u"]
     if letra.strip().lower() in vogais:
         return f"{letra.strip()} é uma vogal."
@@ -12,6 +20,7 @@ def valida_letra_se_eh_vogal_ou_consoante(letra: str) -> str:
 
 
 def test_valida_letra_se_eh_vogal_ou_consoante() -> None:
+    """assert valida_letra_se_eh_vogal_ou_consoante(letra) -> letra_esperada"""
     letra = "a"
     letra_esperada = f"{letra.strip()} é uma vogal."
     assert valida_letra_se_eh_vogal_ou_consoante(letra) == letra_esperada
