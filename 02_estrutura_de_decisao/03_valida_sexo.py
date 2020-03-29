4396807b57aa00542eb45728505781a146d34aa6
alisonamerico <alison.americo@gmail.com>
2020-03-28 21:26:40 -0300

    exercicio 03_valida_sexo

---
+++ 02_estrutura_de_decisao/03_valida_sexo.py
@@ -0,0 +1,26 @@
+"""
+3. Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+"""
+
+
+def valida_sexo(letra: str) -> str:
+    if letra.upper() == "M":
+        return "Masculino"
+    elif letra.upper() == "F":
+        return "Feminino"
+    else:
+        return "Sexo Inválido"
+
+
+def test_valida_sexo() -> None:
+    letra = "M"
+    valor_esperado = "Masculino"
+    assert valida_sexo(letra) == valor_esperado
+
+
+if __name__ == "__main__":
+    letra = input("Informe 'F' para Feminino ou 'M' para Masculino: ")
+
+    valor_esperado = valida_sexo(letra)
+    print(valor_esperado)
